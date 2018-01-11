853138a4ba9c1c6b1ecea935d8bad0fe075dcc92
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-01-11 03:05:08 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector("#nested .target");
+}
+
+function increaseRankBy(n) {
+  const rankedLis = document.querySelectorAll("ul.ranked-list li");
+  
+  for (let i = 0; i < rankedLis.length; i++) {
+    rankedLis[i].innerHTML = parseInt(rankedLis[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  var grandDiv = document.querySelector("#grand-node");
+  var iterator = ["#grand-node"];
+  
+  while(grandDiv.hasChildNodes()) {
+    iterator.push("div");
+    var iteratorString = iterator.join(' ')
+    grandDiv = document.querySelector(iteratorString);
+  }
+  return grandDiv.innerHTML;
+}
